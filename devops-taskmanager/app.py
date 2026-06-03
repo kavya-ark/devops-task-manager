5eb969ad9f096136633f5929c87607374f678be9
kavya-ark <kavyakuppusamyy98@gmail.com>
2026-06-03 08:32:32 +0000

    jenkinsfile

--- devops-taskmanager/app.py
+++ devops-taskmanager/app.py
@@ -26,4 +26,4 @@ def delete_task(index):
     return jsonify({"error": "Task not found"}), 404
 
 if __name__ == "__main__":
-    app.run(debug=True) 
+    app.run(host="0.0.0.0", port=5000, debug=True)
